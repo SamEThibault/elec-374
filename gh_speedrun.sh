65412147cc8765dc4245845677efa1a0e3936d8b
Samuel Emard-Thibault <sam5thibault@gmail.com>
2023-01-23 15:50:23 -0500

    hello

--- gh_speedrun.sh
+++ gh_speedrun.sh
@@ -1,1 +1,6 @@
-git add . && git commit -m "speedy af" && git push
+if [ $1 -eq 0 ]
+then
+    git add . && git commit -m "default commit msg" && git push
+else
+        git add . && git commit -m $1 && git push
+fi
