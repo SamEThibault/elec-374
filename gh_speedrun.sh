70bb1fe87c44cb9ee540c3b4081cec96a91d3be7
Samuel Emard-Thibault <sam5thibault@gmail.com>
2023-01-23 15:50:54 -0500

    script

--- gh_speedrun.sh
+++ gh_speedrun.sh
@@ -1,6 +1,1 @@
-if [ $1 -eq 0 ]
-then
-    git add . && git commit -m "default commit msg" && git push
-else
-        git add . && git commit -m $1 && git push
-fi
+git add . && git commit -m $1 && git push
