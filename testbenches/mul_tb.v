8c56f8a7e53bb8f136c1ed0af83db169c4f26fab
Samuel Emard-Thibault <sam5thibault@gmail.com>
2023-02-24 11:41:54 -0500

    multiply revisions

--- testbenches/mul_tb.v
+++ testbenches/mul_tb.v
@@ -1,26 +1,24 @@
 `timescale 1ns/10ps
-module negate_32_bit_tb(input a, output b);
+module mul_tb(input a, output b);
 	
-    wire[31:0] result_hi;
-    wire[31:0] result_lo;
-    wire[31:0] multiplicand;
-    wire[31:0] multiplier;
+    wire[63:0] res;
+    wire[31:0] M;
+    wire[31:0] Q;
 
-    mul mul_test(res_hi, res_lo, multiplicand, multiplier);
+    mul mul_test(res, M, Q);
 
 	initial
 		begin
             // should be 0000 0000
-            multiplicand = 32'h00000001;
-            multiplier = 32'h00000000;
+            M = 32'h00000001;
+            Q = 32'h00000000;
             #20
             // should be 1111 1111
-            multiplicand = 32'h00000001;
-            multiplier = 32'h11111111;
+            M = 32'h00000001;
+            Q = 32'h11111111;
             #20
             // should be 2000 0000
-            multiplicand = 32'h00000002;
-            multiplier = 32'h10000000;
-
+            M = 32'h00000002;
+            Q = 32'h10000000;
 		end
 endmodule
