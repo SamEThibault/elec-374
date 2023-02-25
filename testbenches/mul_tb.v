039e76bdbf35a770b4affc2a2dfa27df27f0144e
Samuel Emard-Thibault <sam5thibault@gmail.com>
2023-02-25 18:11:34 -0500

    tb changes

--- testbenches/mul_tb.v
+++ testbenches/mul_tb.v
@@ -7,21 +7,14 @@ module mul_tb(input a, input b);
     mul mul_test(res, M, Q);
 
     initial begin
-        // should be 1111 1111
-        M = 32'h00000001;
-        Q = 32'hFFFFFFFF;
+        M = 32'h00000002;
+        Q = 32'h00000002;
         #20;
         $display("M = %h, Q = %h, res = %h", M, Q, res);
 
         // should be 0
         M = 32'h00000000;
         Q = 32'h10000000;
-        #20;
-        $display("M = %h, Q = %h, res = %h", M, Q, res);
-
-        // should be 0000 0002
-        M = 32'h00000002;
-        Q = 32'h00000001;
         #20;
         $display("M = %h, Q = %h, res = %h", M, Q, res);
     end
