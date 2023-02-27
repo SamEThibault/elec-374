58d4cd2fba6443640e6097ff6a322cd480eccfa9
Samuel Emard-Thibault <sam5thibault@gmail.com>
2023-02-27 18:35:08 -0500

    working booth algorithm

--- modules/mul.v
+++ modules/mul.v
@@ -1,76 +1,68 @@
-//module to get M partial product given the 32-bit multiplicand and the current code
-module get_partial_product(
-    input [31:0] M,
-    input [2:0] code,
-    output reg [63:0] res
+// module to generate partial product given multiplicand and i-1, i, and i+1 bits
+// each partial product is 64 bits, and will be shifted to properly align it before addition
+module get_partial_product( 
+    output reg [63:0] res,
+    input [31:0] M, 
+    input [2:0] code
 );
-
-    always @(*) begin
-        case (code)
-            3'b000: res = 64'b0;                            // M * 0
-            3'b111: res = 64'b0;
-
-            3'b001: res = $signed(M);                       // M * 1
-            3'b010: res = $signed(M);
-
-            3'b011: res = $signed(M) <<< 1;                 // M * 2
-
-            3'b100:                                         // M * -2
-            begin
-                res = $signed(M);
-                res = $signed(~res + 1'b1);   
-                res = $signed(res) <<< 1;                   
-				end
-            3'b101: res = (~$signed(M)) + 1'b1;             // M * -1
-            3'b110: res = (~$signed(M)) + 1'b1;
-        endcase
-    end
+     
+    always @(*) begin 
+        // M*1 : add a 64-bit sign extended multiplicand
+        if (code == 3'b001 || code == 3'b010) begin
+            res = $signed(M);
+        // M*2 : add 2* 64-bit sign extended multiplicand
+        end else if (code == 3'b011) begin
+            res = $signed(M) << 1;
+        // M*(-2): add (-2)* 64-bit sign extended multiplicand
+        end else if (code == 3'b100) begin
+            res = $signed(~M+1'b1) << 1;
+        // M*(-1): add (-1)* 64-bit sign extended multiplicand
+        end else if (code == 3'b101 || code == 3'b110) begin
+            res = $signed(~M+1'b1);
+        end else begin
+        // for 111 or 000 codes, add a row of 64 zeros
+            res = 64'b0;
+        end
+    end 
 endmodule
 
 
 // the actual multiplication module
 module mul(
-    output [63:0] res,
-    input signed [31:0] M, 
-    input signed [31:0] Q
+    output [31:0] res_hi,
+    output [31:0] res_lo,
+    input [31:0] M, 
+    input [31:0] Q
 );
-    
-    // array of 16 x 64-bit partial products
-    wire [63:0] partial_product [15:0];
+    // temp register to calculate product
+    reg [63:0] res;
 
-    // // base case
-    // get_partial_product pp_base_call(partial_product[0], M, {Q[1], Q[0], 1'b0});
+    // array to store 16 x 64-bit partial products (32-bit multiplier: 16 partial products)
+    wire [63:0] partial_product [15:0]; 
+	 
+    // base case: add one 0 as LSB for first 2 LSB bits of multiplier
+    get_partial_product generate_pp0(partial_product[0], M, {Q[1], Q[0], 1'b0}); 
 
-    // genvar i;
-    // generate
-    //     for (i = 1; i < 16; i = i + 1) begin : get_partial_products
-    //         get_partial_product pp_call(partial_product[i], M, {Q[i+1], Q[i], Q[i-1]});
-    //     end
-    // endgenerate
+    genvar i;
+    generate
+        for (i = 1; i < 16; i = i + 1) 
+        begin : get_partial_products
+            get_partial_product generate_pp(partial_product[i], M, Q[(i*2+1):(i*2-1)]);
+        end
+    endgenerate
 
-    get_partial_product p0(M,{Q[1:0],1'b0},partial_product[0]); 
-    get_partial_product p1(M,Q[3:1],partial_product[1]); 
-    get_partial_product p2(M,Q[5:3],partial_product[2]); 
-    get_partial_product p3(M,Q[7:5],partial_product[3]); 
-	 get_partial_product p4(M,Q[9:7],partial_product[4]); 
-	 get_partial_product p5(M,Q[11:9],partial_product[5]);
-	 get_partial_product p6(M,Q[13:11],partial_product[6]); 
-	 get_partial_product p7(M,Q[15:13],partial_product[7]); 
-	 get_partial_product p8(M,Q[17:15],partial_product[8]); 
-	 get_partial_product p9(M,Q[19:17],partial_product[9]); 
-	 get_partial_product p10(M,Q[21:19],partial_product[10]); 
-	 get_partial_product p11(M,Q[23:21],partial_product[11]); 
-	 get_partial_product p12(M,Q[25:23],partial_product[12]); 
-	 get_partial_product p13(M,Q[27:25],partial_product[13]); 
-	 get_partial_product p14(M,Q[29:27],partial_product[14]); 
-	 get_partial_product p15(M,Q[31:29],partial_product[15]); 
+    // shift and sum up all generated partial products
+	integer j;
+    always @(*) 
+    begin
+	    res = 63'b0; // start result @ 0
+        for (j = 0; j < 16; j = j + 1) 
+        begin
+            // shift each partial product by 2 to align them properly
+            res = res + (partial_product[j] << (j * 2));
+        end
+    end
 
-
-    assign res = partial_product[0] + (partial_product[1] << 2) + (partial_product[2] << 4)
-                + (partial_product[3] << 6) + (partial_product[4] << 8) + (partial_product[5] << 10)
-                + (partial_product[6] << 12) + (partial_product[7] << 14) + (partial_product[8] << 16)
-                + (partial_product[9] << 18) + (partial_product[10] << 20) + (partial_product[11] << 22)
-                + (partial_product[12] << 24) + (partial_product[13] << 26) + (partial_product[14] << 28)
-                + (partial_product[15] << 30);
-
+    assign res_hi = res[63:32];
+    assign res_lo = res[31:0];
 endmodule
--- testbenches/mul_tb.v
+++ testbenches/mul_tb.v
@@ -1,21 +1,21 @@
 `timescale 1ns/10ps
 module mul_tb(input a, input b);
-    wire [63:0] res;
+    wire [31:0] res_hi;
+	 wire [31:0] res_lo;
     reg [31:0] M;
     reg [31:0] Q;
 
-    mul mul_test(res, M, Q);
+    mul mul_test(res_hi, res_lo, M, Q);
 
     initial begin
+		  // should be 4
         M = 32'h00000002;
         Q = 32'h00000002;
         #20;
-        $display("M = %h, Q = %h, res = %h", M, Q, res);
 
-        // should be 0
-        M = 32'h00000000;
-        Q = 32'h10000000;
+		  // should be 2
+        M = 32'hFFFFFFFF;
+        Q = 32'hFFFFFFFE;
         #20;
-        $display("M = %h, Q = %h, res = %h", M, Q, res);
     end
 endmodule
